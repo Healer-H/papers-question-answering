0d9d6aa17e5f8b106d4c819ed7ab1474c8294e4a
Minh Hieu Huynh <22520434@gm.uit.edu.vn>
2024-08-18 16:46:43 +0700

    feat: add chatbot UI

--- app.py
+++ app.py
@@ -5,27 +5,58 @@ import chainlit as cl
 
 @cl.on_chat_start
 async def start():
-    text_content = 'Hello, this is a text element.'
-    elements = [
-        cl.Text(name='simple_text', content=text_content, display='inline'),
-    ]
+    markdown_content = """
+    # Welcome to the Research Chatbot! 📘✨
 
+    ### Our chatbot is designed to assist researchers and students by providing a range of tools to enhance your understanding and interaction with academic papers, especially in the fields of AI, NLP, and LLM. Here's what you can do with our chatbot:
+
+    ### 🔍 Question Answering
+    Upload your research papers, and ask any question. Our chatbot will provide precise answers by understanding the context embedded within your documents.
+
+    ### 📄 Summarization
+    Get concise summaries of lengthy papers to grasp key concepts and findings quickly, saving you time and effort.
+
+    ### 🔄 Flow Capture and Explanation
+    Visualize the argumentative or logical flow of the papers to better understand the progression and structure of complex ideas.
+
+    ### 🔗 Search and Recommendation
+    Use our advanced semantic search to find related papers or let our chatbot recommend articles that align with your current research interests.
+
+    ### 📚 Interactive Explanations
+    Access interactive content like tutorials and diagrams to get clear explanations of sophisticated concepts.
+
+    ### 📝 Shared Annotations and Collaboration Tools
+    Annotate papers, save your notes, and easily share these with peers or collaborators directly through the interface.
+
+    ### 💬 Discussion Threads
+    Start or join discussion threads linked to specific segments of a paper to engage with the academic community and broaden your understanding.
+
+    To begin, simply upload a paper or type in your question or command into the chat. You can also select from the available functionalities displayed as buttons on your screen.
+
+    Let's dive into your research with an enhanced, supportive experience! 🚀
+    """
+
+    markdown_chunks = markdown_content.split('###')
     msg = cl.Message(
-        content='Check out this text element!',
-        elements=elements,
-        author='Chat hehe',
+        content='',
+        author='Bot',
     )
 
+    for chunk in markdown_chunks:
+        await msg.stream_token(chunk)
+
     await msg.send()
-    # await cl.sleep(2)
-    # await msg.remove()
 
 
 @cl.on_message
-async def another(message: cl.Message):
+async def main(message: cl.Message):
     # Your custom logic goes here...
-
-    # Send a response back to the user
+    # Send a response back to the user ...
+    response = 'Here is the response to your message: ' + message.content
+    author = 'Bot'
     await cl.Message(
-        content=f'Received: {message.content}',
+        content=response,
+        author=author,
+        elements=[],
+        actions=[],
     ).send()
